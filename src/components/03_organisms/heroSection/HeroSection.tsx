8934d12321424162f71d7ec7dc122303af9f33db
Tim Weise <tim.weise@porsche.digital>
2023-06-02 10:48:11 +0200

    ✨ feat: start hero implementation
    Co-authored-by: Eduard Itrich <eduard.itrich@porsche.de>

---
+++ src/components/03_organisms/heroSection/HeroSection.tsx
@@ -0,0 +1,44 @@
+import Image, { StaticImageData } from "next/image";
+import s from "./heroSection.module.scss";
+import { Heading } from "../../01_atoms/Heading";
+
+interface HeroSectionProps {
+  title?: string;
+  subtitle?: string;
+  imageSrc: StaticImageData;
+  imageAlt: string;
+}
+
+export const HeroSection: React.FC<HeroSectionProps> = ({
+  title,
+  subtitle,
+  imageSrc,
+  imageAlt,
+}) => {
+  return (
+    <section className={s["hero-container"]}>
+      <div className={s["background-image-container"]}>
+        <Image className={s["hero-image"]} src={imageSrc} alt={imageAlt} fill />
+        <div className={s["image-overlay"]} />
+      </div>
+      <Heading
+        size={{ base: "small", s: "medium" }}
+        align="center"
+        theme="dark"
+        tag="h5"
+        className={s.subtitle}
+      >
+        {subtitle}
+      </Heading>
+      <Heading
+        size="xxl"
+        align="center"
+        theme="dark"
+        tag="h1"
+        className={s.title}
+      >
+        {title}
+      </Heading>
+    </section>
+  );
+};
