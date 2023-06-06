d06301162e925643effa7e0e870c95eb2eceab2b
Eduard Itrich <eduard.itrich@porsche.de>
2023-06-06 06:20:49 +0000

    Declare png as module without assignment
    Signed-off-by: Eduard Itrich <eduard.itrich@porsche.de>
    on-behalf-of: @porscheofficial <open_source_office@porsche.de>
    Signed-off-by: GitHub <noreply@github.com>

--- src/types.scss.d.ts
+++ src/types.scss.d.ts
@@ -4,8 +4,4 @@ declare module "*.module.scss" {
   export default classes;
 }
 
-declare module "*.png" {
-  const value: import("react-native").ImageSourcePropType;
-  // eslint-disable-next-line import/no-default-export
-  export default value;
-}
+declare module "*.png";
