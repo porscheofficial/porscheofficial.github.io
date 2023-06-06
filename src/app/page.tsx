13092fd56f0ccfb971a52478021bb17e7d9184be
Eduard Itrich <eduard.itrich@porsche.de>
2023-06-06 08:51:51 +0000

    Start work on FOSS Movement section
    Signed-off-by: Eduard Itrich <eduard.itrich@porsche.de>
    on-behalf-of: @porscheofficial <open_source_office@porsche.de>
    Signed-off-by: GitHub <noreply@github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { HeroSection } from "../components/03_organisms/heroSection/HeroSection";
 import s from "./page.module.scss";
 import heroImage from "../../public/assets/heroImage2.png";
+import { Textblock } from "../components/02_molecules/textblock/Textblock";
+import FOSSMovement from "./FOSS_MOVEMENT.mdx";
 
 const Home: React.FC = () => {
   return (
@@ -11,6 +13,9 @@ const Home: React.FC = () => {
         imageSrc={heroImage}
         imageAlt="AI generated Porsche Taycan"
       />
+      <Textblock>
+        <FOSSMovement />
+      </Textblock>
     </main>
   );
 };
