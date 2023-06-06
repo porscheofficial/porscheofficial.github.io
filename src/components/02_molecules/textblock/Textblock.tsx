97589829afe48793bfd962e0be95e22871ead0f5
Eduard Itrich <eduard.itrich@porsche.de>
2023-06-06 09:30:56 +0000

    Add Textblock component
    Signed-off-by: Eduard Itrich <eduard.itrich@porsche.de>
    on-behalf-of: @porscheofficial <open_source_office@porsche.de>
    Signed-off-by: GitHub <noreply@github.com>

---
+++ src/components/02_molecules/textblock/Textblock.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { ReactNode } from "react";
+import s from "./textblock.module.scss";
+
+export const Textblock: React.FC<
+  React.PropsWithChildren<{ children?: ReactNode }>
+> = ({ children }) => {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <section className={s["textblock-container"]}>{children}</section>;
+};
