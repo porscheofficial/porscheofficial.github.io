47bcef2900350359d5c7c880d692c5a79f8d57e7
Eduard Itrich <eduard.itrich@porsche.de>
2023-06-06 04:31:48 +0000

    Statically import image
    Signed-off-by: Eduard Itrich <eduard.itrich@porsche.de>
    on-behalf-of: @porscheofficial <open_source_office@porsche.de>
    Signed-off-by: GitHub <noreply@github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { HeroSection } from "../components/03_organisms/heroSection/HeroSection";
 import s from "./page.module.scss";
+import heroImage from "../../public/assets/heroImage2.png";
 
 const Home: React.FC = () => {
   return (
@@ -7,7 +8,7 @@ const Home: React.FC = () => {
       <HeroSection
         title="Porsche Open Source Platform"
         subtitle="FOSS@Porsche"
-        imageSrc="../../public/assets/heroImage2.png"
+        imageSrc={heroImage}
         imageAlt="AI generated Porsche Taycan"
       />
     </main>
