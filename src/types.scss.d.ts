7526daf59e975237240b72f75487acac1cd64692
Eduard Itrich <eduard.itrich@porsche.de>
2023-06-06 05:17:49 +0000

    Declare *.png as TS module
    Signed-off-by: Eduard Itrich <eduard.itrich@porsche.de>
    on-behalf-of: @porscheofficial <open_source_office@porsche.de>
    Signed-off-by: GitHub <noreply@github.com>

--- src/types.scss.d.ts
+++ src/types.scss.d.ts
@@ -3,3 +3,9 @@ declare module "*.module.scss" {
   // eslint-disable-next-line import/no-default-export
   export default classes;
 }
+
+declare module "*.png" {
+  const value: import("react-native").ImageSourcePropType;
+  // eslint-disable-next-line import/no-default-export
+  export default value;
+}
