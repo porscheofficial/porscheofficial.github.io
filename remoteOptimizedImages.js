d242e91b5bacca36a5a6214cbf71b34643e5d315
Eduard <eduard.itrich@porsche.de>
2023-06-12 10:47:13 +0200

    Link to Medium post
    Signed-off-by: Eduard Itrich <eduard.itrich@porsche.de>
    on-behalf-of: @porscheofficial <open_source_office@porsche.de>

--- remoteOptimizedImages.js
+++ remoteOptimizedImages.js
@@ -1,5 +1,5 @@
 module.exports = [
   "https://raw.githubusercontent.com/oss-review-toolkit/ort/main/logos/logo.png",
   "https://raw.githubusercontent.com/porscheofficial/cookie-consent-banner/main/assets/logo.svg",
-  "https://newsroom.porsche.com/.imaging/mte/porsche-templating-theme/image_1290x726/dam/pnr/2021/Innovation/Porsche-launches-new-open-source-initiative/RP_-_Porsche_Taycan_London_Ballet-109.jpeg/jcr:content/RP_-_Porsche_Taycan_London_Ballet-109.jpeg",
+  "https://miro.medium.com/v2/resize:fit:720/0*Bz4OjfKbqsXSsfAI",
 ];
