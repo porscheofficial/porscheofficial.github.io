e1e1de6da6a1c60da4df2f765124707c128a1256
Eduard <eduard.itrich@porsche.de>
2023-06-12 06:39:05 +0200

    Optimize remote images
    Signed-off-by: Eduard Itrich <eduard.itrich@porsche.de>
    on-behalf-of: @porscheofficial <open_source_office@porsche.de>

---
+++ remoteOptimizedImages.js
@@ -0,0 +1,4 @@
+module.exports = [
+  "https://raw.githubusercontent.com/oss-review-toolkit/ort/main/logos/logo.png",
+  "https://raw.githubusercontent.com/porscheofficial/cookie-consent-banner/main/assets/logo.svg",
+];
