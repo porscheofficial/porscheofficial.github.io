a1f63ca5a850577ef827274f9c2d40fb9bcb2397
Patrick Böder <78862895+pboeder@users.noreply.github.com>
2023-06-15 15:57:08 +0200

    :lipstick: chore: replace news image

--- remoteOptimizedImages.js
+++ remoteOptimizedImages.js
@@ -1,6 +1,5 @@
 module.exports = [
   "https://raw.githubusercontent.com/oss-review-toolkit/ort/main/logos/logo.png",
   "https://raw.githubusercontent.com/porscheofficial/cookie-consent-banner/main/assets/logo.svg",
-  "https://miro.medium.com/v2/resize:fit:720/0*Bz4OjfKbqsXSsfAI",
   "https://avatars.githubusercontent.com/u/45687195?s=400&v=4.png",
 ];
