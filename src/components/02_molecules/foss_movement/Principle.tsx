9c088267b7980f60472bff9529d047a571c5ef23
Eduard Itrich <eduard.itrich@porsche.de>
2023-06-15 13:41:46 +0200

    ✨ feat: Restyle FOSS movement section
    Signed-off-by: Eduard Itrich <eduard.itrich@porsche.de>
    on-behalf-of: @porscheofficial <open_source_office@porsche.de>

---
+++ src/components/02_molecules/foss_movement/Principle.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/require-default-props */
+import { PropsWithChildren, ReactNode } from "react";
+import s from "./principle.module.scss";
+import { Tag } from "../../01_atoms/Tag";
+import { Text } from "../../01_atoms/Text";
+
+export interface PrincipleProps {
+  hashtag: string;
+  children: ReactNode;
+}
+
+export const Principle: React.FC<PropsWithChildren<PrincipleProps>> = (
+  props: PrincipleProps
+) => {
+  const { hashtag, children } = props;
+  return (
+    <div className={s.principle}>
+      <Tag theme="dark" color="background-surface" className={s.hashtag}>
+        #{hashtag}
+      </Tag>
+      <Text theme="dark">{children}</Text>
+    </div>
+  );
+};
