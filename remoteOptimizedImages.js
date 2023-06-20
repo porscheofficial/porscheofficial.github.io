38d08324c562b97d83f4d624aacdb2db26060844
Eduard Itrich <eduard.itrich@porsche.de>
2023-06-20 11:27:54 +0200

    💄 style: Finalize adoption of new project card style
    Signed-off-by: Eduard Itrich <eduard.itrich@porsche.de>
    on-behalf-of: @porscheofficial <open_source_office@porsche.de>

--- remoteOptimizedImages.js
+++ remoteOptimizedImages.js
@@ -1,8 +1,8 @@
 module.exports = [
   "https://raw.githubusercontent.com/oss-review-toolkit/ort/main/logos/logo.png",
   "https://raw.githubusercontent.com/porscheofficial/cookie-consent-banner/main/assets/logo.svg",
-  "https://avatars.githubusercontent.com/u/45687195?s=400&v=4.png",
+  "https://raw.githubusercontent.com/porsche-design-system/porsche-design-system/main/packages/crest/src/porsche-crest%403x.png",
   "https://gitlab.com/uploads/-/system/group/avatar/13943452/terraform-icon.png?width=500.png",
   "https://eslint.org/icon-512.png",
-  "https://source.android.com/images/home/billboard-bg.svg",
+  "https://developer.android.com/static/images/brand/Android_Robot.png",
 ];
