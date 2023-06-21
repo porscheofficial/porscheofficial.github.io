1dbf8607f281c29115bd68d48b60d874db34e06a
Marcel Knobelsdorf <marcel.knobelsdorf@porsche.digital>
2023-06-21 10:34:12 +0200

    feat: enlarge news carousel to extended grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,6 +69,7 @@ const Home: React.FC = () => {
           theme="dark"
           alignHeader="center"
           rewind={false}
+          width="extended"
         >
           {news.items.map((media) => (
             <NewsCard
