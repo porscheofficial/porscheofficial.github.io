23fe226b022903825a69ca6bc9badbde633ae86e
Patrick Böder <78862895+pboeder@users.noreply.github.com>
2023-06-21 17:29:18 +0200

    :white_check_mark: test: add a11y tests for all pages | pbo

--- tests/a11y.spec.ts
+++ tests/a11y.spec.ts
@@ -1,18 +1,36 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, Page } from "@playwright/test";
 import AxeBuilder from "@axe-core/playwright";
 
-test.describe("homepage", () => {
-  test("should not have any automatically detectable accessibility issues", async ({
-    page,
-  }) => {
-    await page.goto("https://porscheofficial-github-io.vercel.app");
+const BASE_URL = "https://porscheofficial-github-io.vercel.app";
 
-    await page.getByLabel("VISITOR PASSWORD").fill("foss");
-    await page.click("button.submit");
-    const accessibilityScanResults = await new AxeBuilder({ page })
-      .withTags(["wcag2a", "wcag21a", "best-practice", "wcag2aa", "wcag21aa"])
-      .analyze();
+const runA11yScan = async (page: Page, path: string): Promise<void> => {
+  await page.goto(`${BASE_URL}${path}`);
 
-    expect(accessibilityScanResults.violations).toEqual([]);
+  await page.getByLabel("VISITOR PASSWORD").fill("foss");
+  await page.click("button.submit");
+
+  // The Carousel does not have a role attribute, so we need to wait for it to be visible
+  await page.waitForLoadState("networkidle");
+
+  // Run the accessibility scan
+  const accessibilityScanResults = await new AxeBuilder({ page })
+    .withTags(["wcag2a", "wcag21a", "best-practice", "wcag2aa", "wcag21aa"])
+    .analyze();
+
+  expect(accessibilityScanResults.violations).toEqual([]);
+};
+
+test.describe("Should not find any automatically detectable accessiblity issues", () => {
+  test("on Homepage", async ({ page }) => {
+    await runA11yScan(page, "/");
+  });
+  test("on Contributing", async ({ page }) => {
+    await runA11yScan(page, "/docs/contributing");
+  });
+  test("on Creating", async ({ page }) => {
+    await runA11yScan(page, "/docs/creating");
+  });
+  test("on Cla", async ({ page }) => {
+    await runA11yScan(page, "/docs/cla");
   });
 });
