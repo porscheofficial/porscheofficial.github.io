d628cafcae7727794c6398453580fb66c5f52a59
Eduard Itrich <eduard.itrich@porsche.de>
2023-07-14 13:59:23 +0200

    🐛 fix: Correctly build edit URL for docs (#9)
    Signed-off-by: Eduard Itrich <eduard.itrich@porsche.de>
    on-behalf-of: @porscheofficial <open_source_office@porsche.de>

--- src/components/02_molecules/editOnGithub/EditOnGithub.tsx
+++ src/components/02_molecules/editOnGithub/EditOnGithub.tsx
@@ -7,15 +7,15 @@ export interface EditOnGithubProps {
 }
 
 export const EditOnGithub: React.FC<EditOnGithubProps> = ({ file }) => {
-  const url = path.join(
-    "https://github.com/porscheofficial/porscheofficial.github.io/edit/main/",
-    file
+  const url = new URL(
+    path.join("porscheofficial/porscheofficial.github.io/edit/main/", file),
+    "https://github.com/"
   );
   return (
     <div className={s["editOnGithub-container"]}>
       <LinkButton
         className={s.editOnGithub}
-        href={url}
+        href={url.href}
         variant="secondary"
         iconSource="/assets/octicons/mark-github.svg"
         theme="dark"
