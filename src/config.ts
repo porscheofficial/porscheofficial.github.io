41dc234caed4aeb47d882b1555cf81c47d781585
Marcel Knobelsdorf <marcel.knobelsdorf@porsche.digital>
2023-11-07 16:48:13 +0100

    chore: clean up baseUrl

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,4 @@
 export const BASE_URL =
-  process.env.BASE_URL ?? process.env.VERCEL_URL ?? "opensource.porsche.com";
+  process.env.BASE_URL ??
+  process.env.VERCEL_URL ??
+  "https://opensource.porsche.com";
