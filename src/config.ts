4cb514fcb7dfe8c9b8838e1c7feee54cd8087d8c
Marcel Knobelsdorf <marcel.knobelsdorf@porsche.digital>
2023-11-08 15:53:45 +0100

    fix: add vercel url fallback | mkn, poe

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
+const VERCEL_URL = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : null;
 export const BASE_URL =
-  process.env.BASE_URL ??
-  process.env.VERCEL_URL ??
-  "https://opensource.porsche.com";
+  process.env.BASE_URL ?? VERCEL_URL ?? "https://opensource.porsche.com";
