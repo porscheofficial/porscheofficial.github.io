1ac6cca394b5f60920d1f4b5a89e8a69b624292b
Eduard Itrich <eduard.itrich@porsche.de>
2024-02-01 08:58:53 +0100

    Set heading as slotted component to ensure accessibility
    Signed-off-by: Eduard Itrich <eduard.itrich@porsche.de>
    on-behalf-of: @porscheofficial <open_source_office@porsche.de>

--- src/components/03_organisms/blogsSection/BlogsSection.tsx
+++ src/components/03_organisms/blogsSection/BlogsSection.tsx
@@ -42,14 +42,13 @@ export const BlogsSection: React.FC<BlogsSectionProps> = ({
         <Section grid={false} spacing="l">
           <PCarousel
             slidesPerPage={{ base: 1, s: 2, l: 3 }}
-            // TODO heading has to be set via property or named slot for component p-carousel in order to ensure accessibility.
-            heading={showHeading ? "Contributor Stories" : ""}
             theme="dark"
             width="extended"
             alignHeader="center"
             rewind={false}
             className={`${className ?? ""}`}
           >
+            <h2 slot="heading">{showHeading ? "Contributor Stories" : ""}</h2>
             {docList.map((blog) => (
               <BlogCard
                 title={blog.title}
