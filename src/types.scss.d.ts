e01146768da7b107d0335667c04bc06c4bfb4ba1
Eduard Itrich <eduard.itrich@porsche.de>
2024-02-01 09:13:38 +0100

    🚨 Fix linter warnings
    Signed-off-by: Eduard Itrich <eduard.itrich@porsche.de>
    on-behalf-of: @porscheofficial <open_source_office@porsche.de>

--- src/types.scss.d.ts
+++ src/types.scss.d.ts
@@ -5,7 +5,9 @@ declare module "*.module.scss" {
 }
 
 declare module "*.yml" {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const data: any;
+  // eslint-disable-next-line import/no-default-export
   export default data;
 }
 
